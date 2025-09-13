fe0d377cec085806b4f6d2b44d0bfdfa13f64a5c
rootVIII <colleyloyejames@gmail.com>
2025-09-13 15:57:06 -0400

    add generator and paginate through gists

--- gist_list.py
+++ gist_list.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser
 from json import loads
 from os import environ
 from pprint import pprint
-from typing import Any
+from typing import Any, Generator
 from urllib.request import Request, urlopen
 
 
@@ -19,11 +19,22 @@ class GistList:
             'Authorization': f'Bearer {environ['GIST_TOK']}'
         }
 
-    def get_gists(self) -> list[dict[str, Any]]:
+    def get_gists(self, url: str) -> list[dict[str, Any]]:
         """Make API call to Github API to retrieve Gists."""
-        req = Request(self.url, headers=self.headers)
+        req = Request(url, headers=self.headers)
         with urlopen(req) as response:
             return loads(response.read().decode('utf-8'))
+
+    def paginate_gists(self) -> Generator[list[dict]]:
+        """Make API call to Github API to retrieve Gists."""
+        index = 1
+        resp = self.get_gists(f'{self.url}?page={index}&per_page=100')
+        index += 1
+        yield resp
+        while resp:
+            resp = self.get_gists(f'{self.url}?page={index}&per_page=100')
+            index += 1
+            yield resp
 
     def parse_gist(self, gist: dict[str, Any]) -> dict[str, Any]:
         """Extract the required keys/values from a given gist dictionary."""
@@ -37,30 +48,32 @@ class GistList:
             'url': gist['html_url']
         }
 
-    def get_gist_list(self) -> list[dict]:
-        return [self.parse_gist(gist) for gist in self.get_gists()]
+    def process_gists(self) -> Generator[dict[str, Any]]:
+        for page in self.paginate_gists():
+            for gist in page:
+                yield self.parse_gist(gist)
 
-    def search_gists_txt(self, text: str) -> list[dict]:
-        matches = []
-        for gist in self.get_gists():
+    def get_gist_list(self):
+        _ = [pprint(gist) for gist in self.process_gists()]
+
+    def search_gists_txt(self, text: str):
+        for gist in self.process_gists():
             gist = self.parse_gist(gist)
-            if text.lower() in gist['description'].lower() \
-                    or text.lower() in gist['filename'].lower() \
-                    or text.lower() in gist['language'].lower():
-                matches.append(gist)
-        return matches
+            val = text.lower()
+            if val in gist['description'].lower() or val in gist['filename'].lower() \
+                    or val in gist['language'].lower() or val in gist['type']:
+                pprint(gist)
 
-    def search_gists_ext(self, text: str) -> list[dict]:
-        matches = []
-        for gist in self.get_gists():
+    def search_gists_ext(self, text: str):
+        for gist in self.process_gists():
             gist = self.parse_gist(gist)
             if gist['description'].lower().endswith(text.lower()):
-                matches.append(gist)
-        return matches
+                pprint(gist)
 
 
 if __name__ == "__main__":
     message = 'Usage: python3 gist_list.py -a | -t <search text> | -e <search extension>'
+    message += '(select 1 option: -a|-t|-e)'
     parser = ArgumentParser(description=message)
     parser.add_argument(
         '-t', '--txt', help='Text to search for'
@@ -69,18 +82,18 @@ if __name__ == "__main__":
         '-e', '--ext', help='File extension to search for'
     )
     parser.add_argument(
-        '-a', '--all', action='store_true', help='Github Username'
+        '-a', '--all', action='store_true', help='Retrieve all gists'
     )
 
     args = parser.parse_args()
     try:
         client = GistList()
         if args.all:
-            pprint(client.get_gist_list())
+            client.get_gist_list()
         elif args.txt:
-            pprint(client.search_gists_txt(args.txt))
+            client.search_gists_txt(args.txt)
         elif args.ext:
-            pprint(client.search_gists_ext(args.ext))
+            client.search_gists_ext(args.ext)
         else:
             parser.print_help()
             raise RuntimeError('No command line options provided.')
